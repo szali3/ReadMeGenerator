2bbc41f29aa84fca157407233ac44d1e2eb9c156
Zohaib Ali <s.zohaibali3@gmail.com>
2021-06-19 15:11:55 -0400

    added readme generator logic

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,90 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const {generateMarkdown,renderLicenseSection,renderLicenseLink, renderLicenseBadge} = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["Enter project title",
+                    "Enter project description",
+                    "Enter installation instructions",
+                    "Enter usage information",
+                    "Enter contribution guidelines",
+                    "Enter test instructions",
+                    "Enter GitHub username",
+                    "Enter email address",
+                    "Enter License"
+                ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
+
+function promptUser (questions) {
+    inquirer.prompt([
+        {
+          type: 'input',
+          name: 'title',
+          message: questions[0]
+        },
+        {
+          type: 'input',
+          name: 'projDescription',
+          message: questions[1]
+        },
+        {
+          type: 'input',
+          name: 'projInstall',
+          message: questions[2]
+        },
+        {
+          type: 'input',
+          name: 'projUsage',
+          message: questions[3]
+        },
+        {
+          type: 'input',
+          name: 'projContribute',
+          message: questions[4]
+        },
+        {
+          type: 'input',
+          name: 'projInstruction',
+          message: questions[5]
+        },
+        {
+          type: 'input',
+          name: 'projGithub',
+          message:  questions[6]
+        },
+        {
+          type: 'input',
+          name: 'projEmail',
+          message:  questions[7]
+        },
+        {
+          type: 'checkbox',
+          name: 'license',
+          message:  questions[8],
+          choices: ['Apache 2.0', 
+            'GNU General Public V3.0', 
+            'MIT',
+            'BSD 3-Clause',
+            'Creative Commons Zero', 
+            'Eclipse Public License', 
+            'GNU Lesser General Public License', 
+            'Mozilla Public License', 
+            'The Unlicense']
+        }
+      ]).then(function(data){
+            generateMarkdown(data)
+      }).catch(err => console.log(err));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    promptUser(questions);
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,103 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  console.log(license);
+  if (license =='Apache 2.0' ){
+      return ` ![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`
+  } else if (license == 'GNU General Public V3.0') {
+      return `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+  } else if (license == 'MIT') {
+      return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  } else if (license =='BSD 3-Clause') {
+      return `![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)`
+  } else if (license =='Creative Commons Zero') {
+      return `![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)`
+  } else if (license =='Eclipse Public License') {
+      return  `![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)`
+  } else if (license =='GNU Lesser General Public License') {
+      return `![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)`
+  } else if (license =='Mozilla Public License') {
+      return  `![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)`
+  }else if (license =='The Unlicense') {
+      return `![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)`;
+  } else {
+      return "";
+  }
 
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  console.log(license);
+  if (license =='Apache 2.0'){
+      return `[${license}]((https://opensource.org/licenses/Apache-2.0))`
+  } else if (license == 'GNU General Public V3.0') {
+      return `[${license}]((https://www.gnu.org/licenses/gpl-3.0.en.html)`
+  } else if (license == 'MIT') {
+      return `[${license}]((https://opensource.org/licenses/MIT))`
+  } else if (license == 'BSD 3-Clause') {
+      return `[${license}]((https://opensource.org/licenses/BSD-3-Clause))`
+  } else if (license == 'Creative Commons Zero') {
+      return `[${license}]((http://creativecommons.org/publicdomain/zero/1.0/))`
+  } else if (license == 'Eclipse Public License') {
+      return `[${license}]((https://opensource.org/licenses/EPL-1.0)`
+  } else if (license == 'GNU Lesser General Public License') {
+      return `[${license}]((https://www.gnu.org/licenses/lgpl-3.0.en.html))`
+  } else if (license == 'Mozilla Public License') {
+      return `[${license}]((https://opensource.org/licenses/MPL-2.0))`
+  } else if (license == 'The Unlicense') {
+      return `[${license}]((http://unlicense.org/))`
+  } else {
+      return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `
+  ## License
+  ### ${renderLicenseLink(license)}
+  `
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+  console.log( 
+    `
+    # ${data.title}
+    ## ${renderLicenseBadge(data.license)}
+
+    ## Description
+    ### ${data.projDescription}
+
+    ## Table of contents
+    ### [Installation](#installation)
+    ### [Usage](#usage)
+    ### [Contributing](#contributing)
+    ### [Tests](#tests)
+    ### [License](#license)
+    ### [Question](#question)
+
+    ## Installation
+    ### ${data.projInstall}
+
+    ## Usage
+    ### ${data.projUsage}
+
+    ## Contributing
+    ### ${data.projContribute}
+
+    ## Tests
+    ### ${data.projTest}
+
+    ${renderLicenseSection(data.license)}
+   
+    ## Question
+    ### Github link${data.projGithub}
+    ### Reach me with additional question${data.projEmail}
+    `);
 }
 
-module.exports = generateMarkdown;
+module.exports = {generateMarkdown,renderLicenseSection,renderLicenseLink, renderLicenseBadge};
